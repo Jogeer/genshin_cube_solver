4a9b6d22ca8e282c030c98983cea50f17c6547ec
Daniil Gorlov <fjshops0@gmail.com>
2021-10-06 07:46:09 +0800

    R

--- genshin_cube_solver.py
+++ genshin_cube_solver.py
@@ -8,6 +8,9 @@ class Cube():
     def __add__(self, other) -> int:
         return self.get_weight() + other.get_weight()
 
+    def __radd__(self, other) -> int:
+        return self.get_weight() + other
+
     def rotate(self, cubes:list, cube_index:int) -> None:
         if self._flag_const:
             cubes[cube_index + 1].rotate(cubes, cube_index + 1)
@@ -61,24 +64,23 @@ class Field():
                 direction) for states, direction in zip(self._userStateMatrix, self._userDirMatrix)]
 
     def _check(self) -> bool:
-        fsum = sum([cube.get_weight() for cube in self._field[:self._matrix_one_size]])
+        fsum = sum([cube for cube in self._field[:self._matrix_one_size]])
 
         for k in range(self._matrix_one_size):
-            temp = sum([cube.get_weight() for cube in self._field[k*self._matrix_one_size:k*self._matrix_one_size+self._matrix_one_size]])
+            temp = sum([cube for cube in self._field[k*self._matrix_one_size:k*self._matrix_one_size+self._matrix_one_size]])
             if temp != fsum:
                 return False
 
         for k in range(self._matrix_one_size):
-            temp = sum([cube.get_weight() for cube in self._field[k::self._matrix_one_size]])
-            ver = sum([self._field[k].get_weight(), self._field[k+self._matrix_one_size].get_weight(), self._field[k+self._matrix_one_size*2].get_weight()])
+            temp = sum([cube for cube in self._field[k::self._matrix_one_size]])
             if temp != fsum:
                 return False
 
-        dig1 = sum([cube.get_weight() for cube in self._field[0::self._matrix_one_size+1]])
+        dig1 = sum([cube for cube in self._field[0::self._matrix_one_size+1]])
         if dig1 != fsum:
             return False
 
-        dig1 = sum([cube.get_weight() for cube in self._field[2:-1:self._matrix_one_size-1]])
+        dig1 = sum([cube for cube in self._field[2:-1:self._matrix_one_size-1]])
         if dig1 != fsum:
             return False
 
@@ -90,7 +92,7 @@ class Field():
     def _get_directions(self) -> list:
         return [cube.get_direction() for cube in self._field]
 
-    def slaver(self):
+    def solve(self) -> None:
         while True:
             if self._check():
                 print(f'Weights: {self._get_weights()}\nDirections: {self._get_directions()}')
@@ -98,7 +100,8 @@ class Field():
             self._field[0].rotate(self._field, 0)
             
 
-def main() -> None: 
+def main() -> None:
+    # Examples:
     # userStateMatrix = ['dynamic', 'consth', 'dynamich', 
     #                    'nothing', 'consth', 'const',
     #                    'dynamic', 'const', 'dynamich']
@@ -120,7 +123,7 @@ def main() -> None:
                      1, 1, 1,
                      2, 3, 1]
 
-    Field(userStateMatrix, userDirMatrix).slaver()
+    Field(userStateMatrix, userDirMatrix).solve()
 
 
 if __name__ == '__main__':
